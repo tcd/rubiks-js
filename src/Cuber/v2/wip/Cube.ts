e60ce1ca0da95a32939f22f10687b9c90718937a
Clay Dunston <claydunston@gmail.com>
2023-02-23 14:31:19 -0600

    [CUBER] `Color`

--- src/Cuber/v2/wip/Cube.ts
+++ src/Cuber/v2/wip/Cube.ts
@@ -1,7 +1,74 @@
+/**
+ * # CUBES
+ *
+ * A Cube is composed of 27 Cubelets (3x3x3 grid) numbered 0 through 26.
+ * Cubelets are numbered beginning from the top-left-forward corner of the
+ * Cube and proceeding left to right, top to bottom, forward to back:
+ *
+ * ```
+ *          -----------------------
+ *        /   18      19      20  /|
+ *       /                       / |
+ *      /   9      10       11  / 20
+ *     /                       /   |
+ *    /   0       1       2   / 11 |
+ *    -----------------------     23
+ *   |                       |2    |
+ *   |   0       1       2   |  14 |
+ *   |                       |    26
+ *   |                       |5    |
+ *   |   3       4       5   |  17 /
+ *   |                       |    /
+ *   |                       |8  /
+ *   |   6       7       8   |  /
+ *   |                       | /
+ *    -----------------------
+ * ```
+ *
+ * Portions of the Cube are grouped (Groups):
+ * - this.core
+ * - this.centers
+ * - this.edges
+ * - this.corners
+ * - this.crosses
+ *
+ * Portions of the Cube are grouped and rotatable (Slices):
+ *
+ * Rotatable around the Z axis:
+ *
+ * - this.front
+ * - this.standing
+ * - this.back
+ *
+ * Rotatable around the X axis:
+ * - this.left
+ * - this.middle
+ * - this.right
+ *
+ * Rotatable around the Y axis:
+ * - this.up
+ * - this.equator
+ * - this.down
+ *
+ * A Cube may be inspected through its Faces (see Slices for more
+ * information on Faces vs Slices). From the browser's JavaScript console:
+ * ```
+ * this.inspect()
+ * ```
+ *
+ * This will reveal each Face's Cubelet indexes and colors using the Face's
+ * compact inspection mode. The non-compact mode may be accessed by passing
+ * a non-false value as an argument:
+ * ```
+ * this.inspect(true)
+ * ```
+ *
+ * @author Mark Lundin - http://www.mark-lundin.com
+ * @author Stewart Smith
+ */
 export class Cube {
 
     constructor() {
-
     }
 
 }
---
+++ src/Cuber/v2/wip/ERNO/Color.ts
@@ -0,0 +1,98 @@
+/**
+ * COLORS
+ * Here's a little bootstrapping to create our Color constants.
+ * At first it seemed like overkill, but then as the solvers and inspectors
+ * moved forward having these objects available became highly desirable.
+ * Sure, ES5 doesn't really have constants but the all-caps alerts you
+ * to the fact that them thar letiables ought not to be messed with.
+ *
+ * @author Mark Lundin - http://www.mark-lundin.com
+ * @author Stewart Smith
+ */
+export class Color {
+
+    public name: string
+    public initial: string
+    public hex: string
+    /** Foreground */
+    public styleF: string
+    /** Background */
+    public styleB: string
+
+    constructor(name: string, initial: string, hex: string, styleF: string, styleB: string) {
+        this.name = name
+        this.initial = initial
+        this.hex = hex
+        this.styleF = styleF
+        this.styleB = styleB
+    }
+}
+
+const _fgWhite = "rgba( 255, 255, 255, 0.9 )"
+const _fgBlack = "rgba( 0, 0, 0, 0.5 )"
+
+const WHITE = new Color(
+    "white",
+    "W",
+    "#FFF",
+    "font-weight: bold; color: #888",
+    `background-color: #F3F3F3; color: ${_fgBlack}`,
+)
+
+const ORANGE = new Color(
+    "orange",
+    "O",
+    "#F60",
+    "font-weight: bold; color: #F60",
+    `background-color: #F60; color: ${_fgWhite}`,
+)
+
+const BLUE = new Color(
+    "blue",
+    "B",
+    "#00D",
+    "font-weight: bold; color: #00D",
+    `background-color: #00D; color: ${_fgWhite}`,
+)
+
+const RED = new Color(
+    "red",
+    "R",
+    "#F00",
+    "font-weight: bold; color: #F00",
+    `background-color: #F00; color: ${_fgWhite}`,
+)
+
+const GREEN = new Color(
+    "green",
+    "G",
+    "#0A0",
+    "font-weight: bold; color: #0A0",
+    `background-color: #0A0; color: ${_fgWhite}`,
+)
+
+const YELLOW = new Color(
+    "yellow",
+    "Y",
+    "#FE0",
+    "font-weight: bold; color: #ED0",
+    `background-color: #FE0; color: ${_fgBlack}`,
+)
+
+const COLORLESS = new Color(
+    "NA",
+    "X",
+    "#DDD",
+    "color: #EEE",
+    "color: #DDD",
+)
+
+export const COLORS = {
+    WHITE,
+    ORANGE,
+    BLUE,
+    RED,
+    GREEN,
+    YELLOW,
+    COLORLESS,
+}
